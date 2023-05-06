a137add45ded43d79bf25afb170e39f95b87e9ea
Joao Lucas <joaolucasbezerra02@gmail.com>
2023-05-06 11:56:38 -0300

    create register form

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 export default function Login() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
     <Container>
-      <h1>Login</h1>
+      <h1>Login {email}</h1>
+
+      <Form>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+      </Form>
     </Container>
   );
 }
